3c2a0cf755a8863ed9c644f34f205f349fc78ba1
kakimaru <memimei0306@gmail.com>
2024-07-31 08:29:37 +0900

    tried ex

--- app.js
+++ app.js
@@ -3,31 +3,41 @@
 $(function() {
     // Exercise 1: Find all <li> elements within a <ul> and change their text color to blue
     $("#exercise1Button").on('click', function() {
-      $("#exercise1 ul").
+      $("#exercise1 ul").children().css({
+        'color' : 'blue'
+      })
     });
 
     // Exercise 2: Find the parent <div> of a paragraph and change its background color
     $("#exercise2Button").on('click', function() {
-      $("#exercise2 p").
+      $("#exercise2 p").parent().css({
+        'background-color': 'green'
+      })
     });
 
     // Exercise 3: Find all ancestor <div> elements of a paragraph and change their background color
     $("#exercise3Button").on('click', function() {
-      $("#exercise3 p").
+      $("#exercise3 p").parents('div').css({
+        'background-color': 'purple'
+      })
     });
 
     // Exercise 4: From the `this` selector, find and hide only the second <p> sibling of the <h3> element. The `this` keyword refers to the clicked button
     $("#exercise4Button").on('click', function() {
-      $(this).
+      $(this).siblings('p:last-of-type').hide()
     });
 
     // Exercise 5: From the `this` selector, find all <p> elements within the <div> and add a border to them. The `this` keyword refers to the clicked button
     $("#exercise5Button").on('click', function() {
-      $(this).
+      $(this).next().children().css({
+        'border' : '1px solid black'
+      })
     });
 
     // Exercise 6: From the `this` selector, find and change the color of <span>Item 5</span>. The `this` keyword refers to the clicked button
     $("#exercise6Button").on('click', function() {
-      $(this).
+      $(this).parent().children('div').last().children().last().find('span').css({
+        'color' : 'red'
+      })
     });
 })
